f84fe105486462e68a39720c5ad58b94aaa00361
JamalS22 <115130666+JamalS22@users.noreply.github.com>
2022-10-06 00:13:17 -0400

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,9 @@
+public class Lab3 {
+
+    public static void main (String[] args) {
+    float fahrenheit = 98.6f;
+    float centigrade =(fahrenheit-32) * 5/9;
+    System.out.println("fahrenheit: " + fahrenheit);
+    System.out.println("centigrade: " + centigrade);
+    }
+}
---
+++ Lab3p2.java
@@ -0,0 +1,11 @@
+public class Lab3p2 {
+
+    public static void main (String[] args) {
+    for (int i = 0; i <=40; i+=5){
+    float fahrenheit = i;
+    float centigrade =(fahrenheit-32) * 5/9;
+    System.out.println("fahrenheit: " + fahrenheit);
+    System.out.println("centigrade: " + centigrade);
+        }
+    }
+}
